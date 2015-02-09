4610157d3cda340518a4ccb64786076875d1d17c
Zylex <zylex@gmail.com>
2015-02-09 02:17:19 +0000

    Displays a red triangle

--- src/Scenes/GlobeScene.cpp
+++ src/Scenes/GlobeScene.cpp
@@ -1,31 +1,35 @@
-#include "GlobeScene.h"
+// #include "GlobeScene.h"
 
-GlobeScene::GlobeScene() {
-  // constructor
-}
+// GlobeScene::GlobeScene() {
+//   // constructor
+// }
 
-GlobeScene::~GlobeScene() noexcept {
-  // destructor
-}
+// GlobeScene::~GlobeScene() noexcept {
+//   // destructor
+// }
 
-GlobeScene::GlobeScene(const GlobeScene& other) {
-  // copy constructor
-}
+// GlobeScene::GlobeScene(const GlobeScene& other) {
+//   // copy constructor
+// }
 
-GlobeScene& GlobeScene::operator=(const GlobeScene& rhs) {
-  // assignement operator
-  if (this == &rhs) {
-    return *this;
-  }
+// GlobeScene& GlobeScene::operator=(const GlobeScene& rhs) {
+//   // assignement operator
+//   if (this == &rhs) {
+//     return *this;
+//   }
 
-  return *this;
-}
+//   return *this;
+// }
 
-GlobeScene::GlobeScene(GlobeScene&& other) noexcept {
-  // move constructor (C++11)
-}
+// GlobeScene::GlobeScene(GlobeScene&& other) noexcept {
+//   // move constructor (C++11)
+// }
 
-GlobeScene& GlobeScene::operator=(GlobeScene&& other) noexcept {
-  // move assignment operator (C++11)
-  return *(new GlobeScene(other));
-}
+// GlobeScene& GlobeScene::operator=(GlobeScene&& other) noexcept {
+//   // move assignment operator (C++11)
+//   return *(new GlobeScene(other));
+// }
+
+// void GlobeScene::processEvent(const zge::Event event) const {
+//   // process event
+// }
--- src/Scenes/GlobeScene.h
+++ src/Scenes/GlobeScene.h
@@ -1,24 +1,25 @@
-#ifndef GLOBESCENE_H
-#define GLOBESCENE_H
+// #ifndef GLOBESCENE_H
+// #define GLOBESCENE_H
 
-namespace zge {
-class IGameState {};
-}
+// #include "IGameState.h"
+// #include "Event.h"
 
-class GlobeScene : public zge::IGameState {
-public:
-  // constructor
-  GlobeScene();
-  // destructor
-  virtual ~GlobeScene() noexcept;
-  // copy constructor
-  GlobeScene(const GlobeScene& other);
-  // assignment operator
-  GlobeScene& operator=(const GlobeScene& other);
-  // move constructor (C++11)
-  GlobeScene(GlobeScene&& other) noexcept;
-  // move assignment operator (C++11)
-  GlobeScene& operator=(GlobeScene&& other) noexcept;
-};
+// class GlobeScene : public zge::IGameState {
+// public:
+//   // constructor
+//   GlobeScene();
+//   // destructor
+//   virtual ~GlobeScene() noexcept;
+//   // copy constructor
+//   GlobeScene(const GlobeScene& other);
+//   // assignment operator
+//   GlobeScene& operator=(const GlobeScene& other);
+//   // move constructor (C++11)
+//   GlobeScene(GlobeScene&& other) noexcept;
+//   // move assignment operator (C++11)
+//   GlobeScene& operator=(GlobeScene&& other) noexcept;
 
-#endif
+//   void processEvent(const zge::Event) const;
+// };
+
+// #endif
